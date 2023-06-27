2352642f72ad8c3c99cd95a3c23b735d85bce007
Ravinder sales <ravinder.y@crowdbotics.com>
2023-06-27 15:30:24 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,20 +1,66 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from "react-native";
 
-const Untitled4 = () => {
-  return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
-      padding: 10,
-      position: 'relative',
-      flex: 1
-    }}></ScrollView>
-    </SafeAreaView>;
+const LoginScreen = () => {
+  return <View style={styles.container}>
+      <Text style={styles.logo}>My App</Text>
+      <View style={styles.inputView}>
+        <TextInput style={styles.inputText} placeholder="Email" placeholderTextColor="#003f5c" />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput style={styles.inputText} placeholder="Password" placeholderTextColor="#003f5c" secureTextEntry />
+      </View>
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>;
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    height: '100%'
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height: 50,
+    color: "black"
+  },
+  forgot: {
+    color: "black",
+    fontSize: 11
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  loginText: {
+    color: "white"
   }
 });
-export default Untitled4;
+export default LoginScreen;
